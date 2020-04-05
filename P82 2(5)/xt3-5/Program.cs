debaf522a939e6196f3206ff4f91b9755f0c6733
zmjheart <1103808379@qq.com>
2020-04-05 14:28:16 +0800

    九九乘法表

---
+++ P82 2(5)/xt3-5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xt3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)  
+            {
+                for (int j = 1; j < 10; j++)   
+
+                {
+                    if (j <= i)
+
+                    {
+                        Console.Write("{0}*{1}={2} \t", i, j, i * j);  
+
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
