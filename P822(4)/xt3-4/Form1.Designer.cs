12268b0f8d85c9402017310bdfa89d413834d54d
zmjheart <1103808379@qq.com>
2020-04-05 14:23:13 +0800

    P82 (4)

---
+++ P822(4)/xt3-4/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace xt3_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum3 = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.btn_compare = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.min = new System.Windows.Forms.Label();
+            this.max = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(154, 63);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 21);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(154, 112);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 21);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // txtNum3
+            // 
+            this.txtNum3.Location = new System.Drawing.Point(154, 163);
+            this.txtNum3.Name = "txtNum3";
+            this.txtNum3.Size = new System.Drawing.Size(100, 21);
+            this.txtNum3.TabIndex = 2;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(281, 232);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 21);
+            this.txtMin.TabIndex = 3;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(281, 278);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 21);
+            this.txtMax.TabIndex = 4;
+            // 
+            // btn_compare
+            // 
+            this.btn_compare.Location = new System.Drawing.Point(372, 112);
+            this.btn_compare.Name = "btn_compare";
+            this.btn_compare.Size = new System.Drawing.Size(75, 23);
+            this.btn_compare.TabIndex = 5;
+            this.btn_compare.Text = "button1";
+            this.btn_compare.UseVisualStyleBackColor = true;
+            this.btn_compare.Click += new System.EventHandler(this.btn_compare_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "请输入三个整数";
+            // 
+            // min
+            // 
+            this.min.AutoSize = true;
+            this.min.Location = new System.Drawing.Point(178, 235);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(41, 12);
+            this.min.TabIndex = 7;
+            this.min.Text = "label2";
+            // 
+            // max
+            // 
+            this.max.AutoSize = true;
+            this.max.Location = new System.Drawing.Point(180, 287);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(41, 12);
+            this.max.TabIndex = 8;
+            this.max.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_compare);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.txtNum3);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum3;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Button btn_compare;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label min;
+        private System.Windows.Forms.Label max;
+    }
+}
+
---
+++ P822(4)/xt3-4/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xt3_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_compare_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int num3 = int.Parse(txtNum3.Text);
+            int[] numbers = new int[3] { num1, num2, num3 };
+            int max = num1, min = num1;
+            //max = num1 > num2 ? num1 : num2;
+            //max = num3 > max ? num3 : max;
+
+            for (int i = 1; i <= 2; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+            txtMin.Text = min.ToString();
+            txtMax.Text = max.ToString();
+        }
+    }
+}
