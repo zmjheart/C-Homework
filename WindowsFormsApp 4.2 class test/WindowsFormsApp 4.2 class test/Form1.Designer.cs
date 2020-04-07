37eb8df8d79510fbf6d1220b4b14339f27371756
zmjheart <1103808379@qq.com>
2020-04-07 18:14:22 +0800

    2020.4.7 class 4.2 myself test

---
+++ WindowsFormsApp 4.2 class test/WindowsFormsApp 4.2 class test/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormsApp_4._2_class_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.rad1 = new System.Windows.Forms.RadioButton();
+            this.rad2 = new System.Windows.Forms.RadioButton();
+            this.grp1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.grp1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "显示(&s)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rad1
+            // 
+            this.rad1.AutoSize = true;
+            this.rad1.Location = new System.Drawing.Point(15, 20);
+            this.rad1.Name = "rad1";
+            this.rad1.Size = new System.Drawing.Size(95, 16);
+            this.rad1.TabIndex = 1;
+            this.rad1.Text = "radioButton2";
+            this.rad1.UseVisualStyleBackColor = true;
+            // 
+            // rad2
+            // 
+            this.rad2.AutoSize = true;
+            this.rad2.Location = new System.Drawing.Point(15, 52);
+            this.rad2.Name = "rad2";
+            this.rad2.Size = new System.Drawing.Size(95, 16);
+            this.rad2.TabIndex = 2;
+            this.rad2.TabStop = true;
+            this.rad2.Text = "radioButton2";
+            this.rad2.UseVisualStyleBackColor = true;
+            // 
+            // grp1
+            // 
+            this.grp1.Controls.Add(this.rad1);
+            this.grp1.Controls.Add(this.rad2);
+            this.grp1.Location = new System.Drawing.Point(81, 67);
+            this.grp1.Name = "grp1";
+            this.grp1.Size = new System.Drawing.Size(200, 100);
+            this.grp1.TabIndex = 3;
+            this.grp1.TabStop = false;
+            this.grp1.Text = "GroupBox分组框容器";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(424, 87);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(100, 56);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.UseSystemPasswordChar = true;
+            this.textBox1.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.grp1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grp1.ResumeLayout(false);
+            this.grp1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rad1;
+        private System.Windows.Forms.RadioButton rad2;
+        private System.Windows.Forms.GroupBox grp1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
