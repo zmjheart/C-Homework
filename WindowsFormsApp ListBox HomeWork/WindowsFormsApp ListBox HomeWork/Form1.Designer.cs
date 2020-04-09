cd4e02d09deeda1230999a76598fe709ea99e305
zmjheart <1103808379@qq.com>
2020-04-09 12:57:51 +0800

    2020.4.9 ListBox Homework

---
+++ WindowsFormsApp ListBox HomeWork/WindowsFormsApp ListBox HomeWork/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp_ListBox_HomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lst1 = new System.Windows.Forms.ListBox();
+            this.modify = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(139, 65);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 21);
+            this.txt1.TabIndex = 0;
+            // 
+            // lst1
+            // 
+            this.lst1.FormattingEnabled = true;
+            this.lst1.ItemHeight = 12;
+            this.lst1.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f"});
+            this.lst1.Location = new System.Drawing.Point(139, 128);
+            this.lst1.Name = "lst1";
+            this.lst1.Size = new System.Drawing.Size(120, 88);
+            this.lst1.TabIndex = 1;
+            // 
+            // modify
+            // 
+            this.modify.Location = new System.Drawing.Point(139, 273);
+            this.modify.Name = "modify";
+            this.modify.Size = new System.Drawing.Size(75, 23);
+            this.modify.TabIndex = 2;
+            this.modify.Text = "修改";
+            this.modify.UseVisualStyleBackColor = true;
+            this.modify.Click += new System.EventHandler(this.modify_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.modify);
+            this.Controls.Add(this.lst1);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.ListBox lst1;
+        private System.Windows.Forms.Button modify;
+    }
+}
+
---
+++ WindowsFormsApp ListBox HomeWork/WindowsFormsApp ListBox HomeWork/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_ListBox_HomeWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void modify_Click(object sender, EventArgs e)
+        {
+            int index = lst1.SelectedIndex;
+            lst1.Items[index] = txt1.Text;
+        }
+    }
+}
